49376a02368ec9ef1e9f0fb5805ceedd19f07d9b
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-03 16:34:48 -0500

    first two events created.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// mouseover
+let funBusImg = document.querySelector("header img")
+funBusImg.addEventListener("mouseover",function(){
+    funBusImg.style.transform = "rotate(180deg)"
+})
+
+//mouseout
+funBusImg.addEventListener("mouseout", function(){
+    funBusImg.style.transform = "rotate(0deg)"
+})
