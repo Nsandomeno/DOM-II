ab1444eb61852ae7aebd4e59dbfe88ee699b38f7
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-03 21:05:31 -0500

    Most of MVP

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ let funBusImg = document.querySelector("header img")
 funBusImg.addEventListener("mouseover",function(){
     funBusImg.style.transform = "rotate(180deg)"
 })
+document.querySelectorAll(".btn").forEach(element => {
+    element.addEventListener("mouseover", function(){
+        element.textContent = "Just Click!"
+    })
+})
 
 // mouseout
 funBusImg.addEventListener("mouseout", function(){
@@ -10,13 +15,28 @@ funBusImg.addEventListener("mouseout", function(){
     //funBusImg.style.zIndex = "1000";
 })
 // Try to set navigation bar with a fixed position in front
-document.querySelector(".main-navigation .nav-container").style.zIndex = "1";
+// document.querySelector(".main-navigation .nav-container").display.zIndex = "1";
+
+document.querySelectorAll(".btn").forEach(element => {
+    element.addEventListener("mouseout",function(){
+        element.style.backgroundColor = "red"
+        element.textContent = "Come Back!"
+    })
+})
+
+
 
 // click
 let logoHeading = document.querySelector(".logo-heading");
 logoHeading.addEventListener("click", function(){
     logoHeading.textContent = "WELCOME TO THE BUS"
     logoHeading.style.color = "yellow";  
+})
+
+let secondHeads = document.querySelectorAll(".text-content h2").forEach(element =>{
+    element.addEventListener("click",function(){
+        element.textContent = "This is an h2";
+    })
 })
 
 // keydown
@@ -53,5 +73,10 @@ lastPic.addEventListener("dblclick",function(){
 // resize
 window.addEventListener("resize", function(){
     entire.style.backgroundColor = "hotpink";
+})
+
+let thirdImg = document.querySelector(".img-fluid");
+window.addEventListener("resize", function(){
+    thirdImg.style.display = "none";
 })
 
